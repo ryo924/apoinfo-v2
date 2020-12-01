5726fac75cf5e128b2d1a0f8c76b28d8b9f8407e
ryo924 <ryo.tominaga24@gmail.com>
2020-12-01 18:42:22 +0900

    アポイント登録機能の実装

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,22 @@ class AppointmentsController < ApplicationController
   def index
   end
 
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:title, :company, :customer, :date, :place, :purpose, :relation_id).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "appointments#index"
+  resources :appointments, only: [:new, :create]
 end

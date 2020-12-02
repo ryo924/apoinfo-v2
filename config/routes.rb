b2d2089ad88a740f01d0c59bd8723f1cb3bd88e0
ryo924 <ryo.tominaga24@gmail.com>
2020-12-02 18:59:17 +0900

    コメント作成機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "appointments#index"
-  resources :appointments, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :appointments do
+    resources :comments, only: :create
+  end
 end

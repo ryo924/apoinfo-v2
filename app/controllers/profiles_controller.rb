83635c1d8438005a5cc40b8c5a90b81fcda442d0
ryo924 <ryo.tominaga24@gmail.com>
2020-12-07 12:41:42 +0900

    ページ遷移のリダイレクト設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,18 @@
 class ProfilesController < ApplicationController
 
+  before_action :set_profile, only: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:new, :create]
+
   def index
-    @appointment = Appointment.find(params[:appointment_id])
     @profiles = Profile.all.order(created_at: "DESC")
   end
 
   def new
-    @appointment = Appointment.find(params[:appointment_id])
     @profile = Profile.new
   end
 
   def create
-    @appointment = Appointment.find(params[:appointment_id])
     @profile = Profile.new(profile_params)
     if @profile.save
       redirect_to appointment_profiles_path(@appointment)
@@ -23,6 +24,14 @@ class ProfilesController < ApplicationController
   private
   def profile_params
     params.require(:profile).permit(:customer_name, :customer_occupation, :position, :note).merge(appointment_id: params[:appointment_id])
+  end
+
+  def set_profile
+    @appointment = Appointment.find(params[:appointment_id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @appointment.user
   end
 
 end
